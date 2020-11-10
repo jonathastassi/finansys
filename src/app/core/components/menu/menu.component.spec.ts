de1e46970f559e3aab6c1cb1907a5b1c6479c1d4
Jonathas <jonathastassi@hotmail.com>
2020-11-10 15:15:39 -0300

    teste de unidade do MenuComponent

--- src/app/core/components/menu/menu.component.spec.ts
+++ src/app/core/components/menu/menu.component.spec.ts
@@ -1,14 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Router, Routes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CategoryListComponent } from './../../../pages/categories/category-list/category-list.component';
+import { EntryListComponent } from './../../../pages/entries/entry-list/entry-list.component';
+import { ReportsComponent } from './../../../pages/reports/reports/reports.component';
 import { MenuComponent } from './menu.component';
 
-describe('MenuComponent', () => {
+
+describe('O MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
+  let router: Router;
+
+  const routes = [
+    {path: 'entries', component: EntryListComponent},
+    {path: 'categories', component: CategoryListComponent},
+    {path: 'reports', component: ReportsComponent},
+  ] as Routes;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      declarations: [ MenuComponent ],
+      imports: [RouterTestingModule.withRoutes(routes)]
     })
     .compileComponents();
   }));
@@ -17,9 +30,41 @@ describe('MenuComponent', () => {
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router = TestBed.inject(Router);
   });
 
-  it('should create', () => {
+  it('deve ser criado', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve redirecionar para a rota /reports quando o menu Relatórios for clicado', fakeAsync(() => {
+
+    const link = fixture.debugElement.nativeElement.querySelector('#navbarNav > ul > li:nth-child(1) > a');
+    link.click();
+
+    tick();
+
+    expect(router.url).toBe('/reports');
+  }));
+
+  it('deve redirecionar para a rota /entries quando o menu Lançamentos for clicado', fakeAsync(() => {
+
+    const link = fixture.debugElement.nativeElement.querySelector('#navbarNav > ul > li:nth-child(2) > a');
+    link.click();
+
+    tick();
+
+    expect(router.url).toBe('/entries');
+  }));
+
+  it('deve redirecionar para a rota /categories quando o menu Categorias for clicado', fakeAsync(() => {
+
+    const link = fixture.debugElement.nativeElement.querySelector('#navbarNav > ul > li:nth-child(3) > a');
+    link.click();
+
+    tick();
+
+    expect(router.url).toBe('/categories');
+  }));
 });

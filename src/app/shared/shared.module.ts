c5fc0256d0221bb07820dd0bf3e1b327cdb778a3
Jonathas Tassi <jonathastassi@hotmail.com>
2020-11-10 11:24:33 -0300

    page header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
-  declarations: [BreadCrumbComponent],
+  declarations: [BreadCrumbComponent, PageHeaderComponent],
   imports: [CommonModule, FormsModule, RouterModule, ReactiveFormsModule],
   exports: [
     CommonModule,
@@ -13,6 +14,7 @@ import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.compon
     RouterModule,
     ReactiveFormsModule,
     BreadCrumbComponent,
+    PageHeaderComponent,
   ],
 })
 export class SharedModule {}
